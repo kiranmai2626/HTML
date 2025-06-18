30bda4bad3c03aa319448febf4962bb81823f383
kiranmai2626 <kiranmai.anagani1@gmail.com>
2025-06-18 14:06:30 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -272,3 +272,200 @@ I N D I A  */
 
 
 
+*/
+public class Pattern {
+    public static void main(String[] args) 
+    {
+        String word = "INDIA";
+        for (int i = 1; i <=5; i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                System.out.print(word.charAt(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+
+*/
+class Patterns
+{
+    public static void main(String[]args)
+    {
+    
+    
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+      * 
+    * * * 
+  * * * * * 
+* * * * * * * 
+  * * * * * 
+    * * * 
+      * 
+*/      
+
+
+public class Pattern {
+    public static void main(String[] args) 
+    {
+        int rows = 4; 
+        for (int i = 1; i <= rows; i++) 
+        {
+
+            for (int j = 1; j <= rows - i; j++) 
+            {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) 
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        for (int i = rows - 1; i >= 1; i--) 
+        {
+            for (int j = 1; j <= rows - i; j++) 
+            {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
+*/
+
+
+
+public class Pattern
+{
+    public static void main(String[] args) 
+    {
+        int size = 5; 
+
+        for (int i = 1; i <= size; i++) 
+        {
+            for (int j = 1; j <= size; j++) 
+            {
+                if (i == 1 || i == size || j == 1 || j == size) 
+                {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  "); 
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+    *     
+    *     
+* * * * * 
+    *     
+    *   
+*/
+
+
+
+public class Pattern 
+{
+    public static void main(String[] args) 
+    {
+        int n = 5; 
+
+        int mid = n / 2 + 1; 
+
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= n; j++) 
+            {
+               
+                if (i == mid || j == mid) 
+                {
+                    System.out.print("* ");
+                } else 
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+/*
+*       * 
+  *   *   
+    *     
+  *   *   
+*       * 
+
+*/
+
+
+
+public class Pattern {
+    public static void main(String[] args) 
+    {
+        int n = 5; 
+
+        for (int i = 1; i <= n; i++) 
+        {         
+            for (int j = 1; j <= n; j++) 
+            {      
+                
+                if (i == j || i + j == n + 1) 
+                {
+                    System.out.print("* ");
+                } else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
