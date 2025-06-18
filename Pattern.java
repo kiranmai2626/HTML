330c656a24903b7e5567e87b8edf1ca1ce885b74
kiranmai2626 <kiranmai.anagani1@gmail.com>
2025-06-18 15:41:33 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -348,7 +348,44 @@ output:
       * 
 */      
 
-       
+ /*13. class Pattern1 {
+ public static void main(String[] args) {
+int n = 5; 
+for (int i = 1; i <= n; i++) {
+ for (int j = i; j < n; j++)
+ System.out.print(" ");
+for (int j = 1; j <= (2 * i - 1); j++) {
+if (j == 1 || j == (2 * i - 1))
+System.out.print("*");
+else
+ System.out.print(" ");
+ }
+ System.out.println();
+  }
+ for (int i = n - 1; i >= 1; i--) {
+for (int j = n; j > i; j--)
+System.out.print(" ");
+for (int j = 1; j <= (2 * i - 1); j++) {
+ if (j == 1 || j == (2 * i - 1))
+  System.out.print("*");
+else
+ System.out.print(" ");
+}
+ System.out.println();
+ }
+}
+}
+output:
+     *
+   * *
+  *   *
+ *     *
+*       *
+ *     *
+  *   *
+   * *
+    *
+*/      
 //14
 
 
@@ -452,4 +489,80 @@ output:
   *   *   
 *       * 
 
+*/
+
+/*
+17.class Pattern1 {
+ public static void main(String[] args) {
+  int size = 7; 
+ int mid = size / 2;
+for (int i = 0; i < size; i++) {
+ for (int j = 0; j < size; j++) {
+if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+ System.out.print("*");
+else if (i == mid || j == mid)
+ System.out.print("*");
+else
+ System.out.print(" ");
+}
+ System.out.println();
+ }
+ }
+}
+output:
+*******
+*    *    *
+*    *    *
+*******
+*    *    *
+*    *    *
+*******
+*/
+/*
+18.class Pattern1 {
+ public static void main(String[] args) {
+        int n = 7; 
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+               
+                if (i == n || j == 1 || j == i)
+                    System.out.print(" * ");
+                else
+                    System.out.print("  ");
+            
+}
+            System.out.println();
+        
+    }
+}
+}
+output:
+*  *  *  *  *  *  *
+ *            *
+ *         *
+ *       *
+ *     *
+ *   *
+ *
+*/
+/*
+19.class Pattern1 {
+public static void main(String[] args) {
+int n = 6; 
+for (int i = 0; i < n; i++) {
+ for (int j = 0; j < i; j++) {
+System.out.print("*");
+ }
+ System.out.println(i);
+}
+}
+}
+output:
+0
+*1
+**2
+***3
+****4
+*****5
 */
