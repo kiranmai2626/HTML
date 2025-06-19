0ed930892bbce8b27e85beaefb99e1d2d21462ec
kiranmai2626 <kiranmai.anagani1@gmail.com>
2025-06-19 11:08:04 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -566,3 +566,33 @@ output:
 ****4
 *****5
 */
+
+ //20
+
+/* class Pattern1 {
+ public static void main(String[] args) {
+        int n = 7; 
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+               
+                if (i==1 || j ==1  || j ==i)
+                    System.out.print(" * ");
+                else
+                    System.out.print("  ");
+            
+}
+            System.out.println();
+        
+    }
+}
+}
+output:
+ *  *  *  *  *  *  *
+ *  *
+ *    *
+ *      *
+ *        *
+ *          *
+ *            *
+  */
